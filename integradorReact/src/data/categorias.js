f6546c67609daf9a328985883bbf4959f69bac14
Micaela Bahurlet <micabahurletgo@gmail.com>
2023-12-27 21:56:51 -0300

    first commit

---
+++ integradorReact/src/data/categorias.js
@@ -0,0 +1,27 @@
+export const categorias = [
+    {
+        id: 1,
+        img: "./data/imgProducts/In-EARcategory.png,",
+        title: "In-ear",
+        category: "In-ear",
+    },
+    {
+        id: 2,
+        img: "./data/imgProducts/gamer-category.png",
+        title: "Gamer",
+        category: "Gamer",
+    },
+    {
+        id: 3,
+        img: "./data/imgProducts/Deportivo-category.png",
+        title: "Deportivo",
+        category: "Deportivo",
+    },
+    {
+        id: 4,
+        img: "./data/imgProducts/Classic-category.png",
+        title: "Classic",
+        category: "Classic",
+    }
+
+]
