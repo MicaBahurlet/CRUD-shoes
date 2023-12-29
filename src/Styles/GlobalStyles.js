9c62c22236fbb43c686023322cb7ed92698d4474
Micaela Bahurlet <micabahurletgo@gmail.com>
2023-12-29 09:30:33 -0300

    init Styles

---
+++ src/Styles/GlobalStyles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle` //siempre la svastik. Y adentro vamos a poner todos los estilos globales. Ver si luego necesito un reset de de box sizing, margin y padding. 
+
+
+
+
+`
