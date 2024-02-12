bd793ce8af1caabefd4ce5c6e4c739d2e8f4c3f6
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-02-12 18:09:12 -0300

    remove StrictMode in Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import { GlobalStyles } from './Styles/GlobalStyles.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <GlobalStyles />
     <App />
-  </React.StrictMode>,
+  </>,
 )
