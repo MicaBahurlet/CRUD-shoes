1125f016224a52081875a85e2f0f6f01c6228c8a
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-04-01 21:13:32 -0300

    finish modal add product in cart

--- src/pags/Products/CardProductos.jsx
+++ src/pags/Products/CardProductos.jsx
@@ -1,4 +1,50 @@
 
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cart/cartSlice';
+import { ModalContainer, ModalContent } from './ModalStyles'; 
+import { CardProducto, 
+  BotonBuy
+  
+} from "./Product"
+
+function Productos({ img, title, category, price, desc, id }) {
+    const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false); // Estado para controlar el modal
+
+    const handleAddToCart = () => {
+        dispatch(addToCart({ img, title, category, price, desc, id }));
+        setShowModal(true); // Mostrar el mensaje cuando se agrega el producto al carrito
+        
+        setTimeout(() => {
+            setShowModal(false);
+        }, 3000); // Ocultar mensaje despues de 3 segundos
+    };
+
+    return (
+        <>
+            <CardProducto>
+                <img src={img} alt={category} />
+                <h3>{title}</h3>
+                <p>{desc}</p>
+                <p>${price}</p>
+                <BotonBuy onClick={handleAddToCart}>Comprar</BotonBuy>
+            </CardProducto>
+
+            {/* Mostrar el modal cuando se agrega el producto al carrito */}
+            {showModal && (
+                <ModalContainer className="addModal activeModal">
+                    <ModalContent>Producto agregado al carrito</ModalContent>
+                </ModalContainer>
+            )}
+        </>
+    );
+}
+
+export default Productos;
+
+
+//esto tenia antes, cualquier cosa borrar lo de arriba y además el ModalStyles. 
 // import { CardProducto, 
 //   BotonBuy
   
@@ -30,49 +76,3 @@
 // }
 
 // export default Productos
-
-
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addToCart } from '../../redux/cart/cartSlice';
-import { ModalContainer, ModalContent } from './ModalStyles'; 
-import { CardProducto, 
-  BotonBuy
-  
-} from "./Product"
-
-function Productos({ img, title, category, price, desc, id }) {
-    const dispatch = useDispatch();
-    const [showModal, setShowModal] = useState(false); // Estado para controlar la visualización del modal
-
-    const handleAddToCart = () => {
-        dispatch(addToCart({ img, title, category, price, desc, id }));
-        setShowModal(true); // Mostrar el modal cuando se agrega el producto al carrito
-        // Puedes agregar un temporizador para ocultar el modal después de cierto tiempo si lo deseas
-        setTimeout(() => {
-            setShowModal(false);
-        }, 3000); // Oculta el modal después de 3 segundos (3000 milisegundos)
-    };
-
-    return (
-        <>
-            <CardProducto>
-                <img src={img} alt={category} />
-                <h3>{title}</h3>
-                <p>{desc}</p>
-                <p>${price}</p>
-                <BotonBuy onClick={handleAddToCart}>Comprar</BotonBuy>
-            </CardProducto>
-
-            {/* Modal */}
-            {showModal && (
-                <ModalContainer className="addModal activeModal">
-                    <ModalContent>Producto agregado al carrito</ModalContent>
-                </ModalContainer>
-            )}
-        </>
-    );
-}
-
-export default Productos;
-
