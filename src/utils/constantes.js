0c2a64a6566562b3ce032e82708ce1802523276f
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-07-28 09:25:23 -0300

    solution. connection successful to back

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,6 +1,6 @@
 export const SHIPPING_COST = 7500; // Costo de envio por defecto
 export const INITIAL_LIMIT = 8; // Cantidad inicial de productos por defecto
-export const BASE_URL = "http://back-crud.vercel.app"; // URL anterior "http://localhost:6060"
+export const BASE_URL = "http://127.0.0.1:6060"; 
 
 export const ADMIN = 'admninRole';
 export const USER = 'userRole';
